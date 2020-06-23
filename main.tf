55eedd57bd14fd008d831a6ace3e8753c582667e
Mantesh Patil <42858444+patmante@users.noreply.github.com>
2020-06-23 17:05:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,42 @@
-# provider "azurerm" {
-#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
-#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
-#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
-#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
-# }
+provider "azurerm" {
+  version = "~> 1.44"
+ subscription_id = var.subscription_id
+   client_id       = var.client_id
+   client_secret   = var.client_secret
+   tenant_id       = var. tenant_id
+ }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_sql_database" "db" {
-  name                             = "${var.db_name}"
-  resource_group_name              = "${azurerm_resource_group.rg.name}"
-  location                         = "${var.location}"
-  edition                          = "${var.db_edition}"
-  collation                        = "${var.collation}"
-  server_name                      = "${azurerm_sql_server.server.name}"
+  name                             = var.db_name
+  resource_group_name              = azurerm_resource_group.rg.name
+  location                         = var.location
+  edition                          = var.db_edition
+  collation                        = var.collation
+  server_name                      = azurerm_sql_server.server.name
   create_mode                      = "Default"
-  requested_service_objective_name = "${var.service_objective_name}"
-  tags                             = "${var.tags}"
+  requested_service_objective_name = var.service_objective_name
+  tags                             = var.tags
 }
 
 resource "azurerm_sql_server" "server" {
-  name                         = "${var.db_name}-sqlsvr"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  location                     = "${var.location}"
-  version                      = "${var.server_version}"
-  administrator_login          = "${var.sql_admin_username}"
-  administrator_login_password = "${var.sql_password}"
-  tags                         = "${var.tags}"
+  name                         = var.db_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.location
+  version                      =var.server_version
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_password
+  tags                         = var.tags
 }
 
 resource "azurerm_sql_firewall_rule" "fw" {
-  name                = "${var.db_name}-fwrules"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  server_name         = "${azurerm_sql_server.server.name}"
-  start_ip_address    = "${var.start_ip_address}"
-  end_ip_address      = "${var.end_ip_address}"
+  name                = var.db_name
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.server.name
+  start_ip_address    = var.start_ip_address
+  end_ip_address      = var.end_ip_address
 }
